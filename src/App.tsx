4ee1e415441ca822031466ad37d1ac52afd99f6f
Junior Oliveira Leandro <contato_jr@outlook.com>
2023-06-28 11:47:11 -0300

    Conexão com API funcional.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 
-import './App.css'
+import './App.css';
+import axios from 'axios';
+import md5 from 'md5';
+import { useEffect } from 'react';
 
-function App() {
 
-  return (
-    <>
-      <div className='content'>
-        <h1>Vite + React | Building Discovery Marvel</h1>
-        <br/>
-        <button type="button" id="discovery">CLICK HERE FOR DISCOVERY</button>
-      </div>
-    </>
+const publicKey = 'process.env.PUBLIC_KEY';
+const privateKey = 'process.env.PRIVATE_KEY';
+
+const time = Number(new Date());
+
+const hash = md5(time + privateKey + publicKey);
+
+console.log(privateKey);
+
+
+ const App: React.FC = () => {
+  useEffect(() => {
+  axios
+  .get(`http://gateway.marvel.com/v1/public/characters?ts=${time}&apikey=${publicKey}&hash=${hash}`,
   )
-}
+  .then (response => console.log(response.data.data))
+  .catch(err => console.log(err));
+ },[]);
+  return ( 
+  <>
+    <div className='content'>
+      <h1>Vite + React | Building Discovery Marvel</h1>
+        <br />
+      <button type="button" id="discovery">CLICK HERE FOR DISCOVERY</button>
+    </div>
+  </>
+  );
+};
 
 export default App
